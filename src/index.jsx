daea999c52efcdf4ba72b3ec49ce82ae60973cfb
adenisovgit <adenisov@gmail.com>
2019-12-16 17:25:04 +0700

    directory renamed App -> app

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import App from './components/App';
+import App from './components/app';
 
 export default ({ channels }) => {
   const store = createStore(
